b6b682d39abb55ee8a9c2863a91fe674348e6341
LucasChr <LucasChr@LucasNot.lucas.chaves>
2016-08-29 10:13:56 -0300

    Novas telas adicionadas

---
+++ restaurantesFamosos/src/main/java/controller/ConfirmaCadastroController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ConfirmaCadastroController {
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TextField tfCodigo;
+
+    @FXML
+    private Button btnEntrar;
+
+    @FXML
+    void Entrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Voltar(ActionEvent event) {
+
+    }
+
+}
