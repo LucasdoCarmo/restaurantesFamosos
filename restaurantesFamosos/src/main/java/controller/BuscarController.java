381812148ca42c9d7539af65128a3aa5bfdbed1d
LucasChr <LucasChr@LucasNot.lucas.chaves>
2016-08-29 18:03:13 -0300

    Novas telas acrescentadas

---
+++ restaurantesFamosos/src/main/java/controller/BuscarController.java
@@ -0,0 +1,96 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class BuscarController {
+
+    @FXML
+    private Button btnAvaliar;
+
+    @FXML
+    private Button btnTema;
+
+    @FXML
+    private Button btnEditar;
+
+    @FXML
+    private ComboBox<?> cbTipo;
+
+    @FXML
+    private Button btnBusca;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private ComboBox<?> cbTema;
+
+    @FXML
+    private Button btnBuscaAvancada;
+
+    @FXML
+    private Button btnTipo;
+
+    @FXML
+    private Button btnExcluir;
+
+    @FXML
+    private TableView<?> tbResultado;
+
+    @FXML
+    private Button btnNome;
+
+    @FXML
+    private TextField tfNome;
+
+    @FXML
+    void Buscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Excluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Editar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Avaliar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Voltar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BuscarNome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BuscaTema(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BuscaTipoEstabelecimento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BuscaAvancada(ActionEvent event) {
+
+    }
+
+}
---
+++ restaurantesFamosos/src/main/java/controller/CadastraRestauranteController.java
@@ -0,0 +1,64 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class CadastraRestauranteController {
+
+    @FXML
+    private ComboBox<?> cbPais;
+
+    @FXML
+    private ComboBox<?> cbCidade;
+
+    @FXML
+    private ComboBox<?> cbEstado;
+
+    @FXML
+    private Button btnAvaliacao;
+
+    @FXML
+    private TextField tfCep;
+
+    @FXML
+    private DatePicker dpVisita;
+
+    @FXML
+    private TextField tfNumero;
+
+    @FXML
+    private TextField tfTelefone;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private ComboBox<?> cbTema;
+
+    @FXML
+    private TextField tfValor;
+
+    @FXML
+    private ComboBox<?> cbTipoEstabelecimento;
+
+    @FXML
+    private TextField tfRua;
+
+    @FXML
+    private TextField tfNome;
+
+    @FXML
+    void Voltar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Avaliacao(ActionEvent event) {
+
+    }
+
+}
