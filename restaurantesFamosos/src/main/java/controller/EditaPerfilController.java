482f2377308f87ee4c492e6d1c0d33c3ae84f333
Lucas <Lucas@172.18.21.101>
2016-10-27 20:57:11 -0200

    Implementado a tela edita perfil

--- restaurantesFamosos/src/main/java/controller/EditaPerfilController.java
+++ restaurantesFamosos/src/main/java/controller/EditaPerfilController.java
@@ -1,15 +1,27 @@
 package controller;
 
+import java.io.IOException;
+import dao.UsuarioDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import model.Usuario;
 
 public class EditaPerfilController {
 
+	@FXML
+    private BorderPane panelSecundario;
+	
     @FXML
     private Button btnConfirmar;
 
+    @FXML
+    private TextField tfCodigo;
+    
     @FXML
     private TextField tfSenha;
 
@@ -25,14 +37,42 @@ public class EditaPerfilController {
     @FXML
     private TextField tfNome;
 
+    private UsuarioDAO usuarioDAO;
+    
     @FXML
     void Voltar(ActionEvent event) {
-
+    	AbreTela("TelaVazia.fxml");
     }
 
     @FXML
     void Confirmar(ActionEvent event) {
-
+    	Usuario usuario = modificaUsuario();
+    	usuarioDAO.salvar(usuario);
     }
-
+    
+    
+	private Usuario modificaUsuario() {
+    	//implementar uma forma de salvar a avaliação do usuário
+		usuarioDAO.excluir(Long.valueOf(tfCodigo.getText()));
+		Usuario usuario = new Usuario();
+		usuario.setNome(tfNome.getText());
+		usuario.setEmail(tfEmail.getText());
+		if(tfConfirmaSenha.getText() == tfSenha.getText()){
+			if(!tfSenha.getText().isEmpty())
+			usuario.setSenha(tfSenha.getText());	
+		}
+		return usuario;
+	}
+    
+	  public void AbreTela(String tela) {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/telas/" +tela));
+			try {
+				AnchorPane telaView = (AnchorPane) loader.load();
+				panelSecundario.setCenter(telaView);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}  
+	
 }
