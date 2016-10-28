c2ac58d7bd0abd53ffac2165b4133ed1a48d861d
Roni <rooni.cardoso@gmail.com>
2016-10-28 09:10:52 -0200

    radio button com os id

--- restaurantesFamosos/src/main/java/controller/AvaliacaoController.java
+++ restaurantesFamosos/src/main/java/controller/AvaliacaoController.java
@@ -10,76 +10,200 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import model.Avaliacao;
 
 public class AvaliacaoController {
 
-    @FXML
-    private ToggleGroup AparenciaInterna;
+	   @FXML
+	    private TextField tfNomeRestaurante;
 
-    @FXML
-    private ToggleGroup Variedade;
+	    @FXML
+	    private RadioButton rbDentroDoEsperado;
 
-    @FXML
-    private ToggleGroup TempoEntrega;
+	    @FXML
+	    private ToggleGroup TempoEntrega;
 
-    @FXML
-    private ToggleGroup QualidadeValorPago;
+	    @FXML
+	    private RadioButton rbSatisfatorio;
 
-    @FXML
-    private ToggleGroup Pagamento;
+	    @FXML
+	    private RadioButton rbOtimo;
 
-    @FXML
-    private ToggleGroup BebidasAlcoolicas;
+	    @FXML
+	    private RadioButton rbMuitoDemorado;
 
-    @FXML
-    private TextArea taDescricao;
+	    @FXML
+	    private RadioButton rbNota2;
 
-    @FXML
-    private Button btnSalvar;
+	    @FXML
+	    private ToggleGroup NotaAtendimento;
 
-    @FXML
-    private ToggleGroup NotaCardapio;
+	    @FXML
+	    private RadioButton rbNota3;
 
-    @FXML
-    private ToggleGroup Espera;
+	    @FXML
+	    private RadioButton rbNota4;
 
-    @FXML
-    private ToggleGroup Limpeza;
+	    @FXML
+	    private RadioButton rbNota5;
 
-    @FXML
-    private Button btnVoltar;
+	    @FXML
+	    private RadioButton rbNota1;
 
-    @FXML
-    private ToggleGroup BebidasSemAlcool;
+	    @FXML
+	    private RadioButton rbRuimIrritante;
 
-    @FXML
-    private ToggleGroup QualidadeAtendimento;
+	    @FXML
+	    private ToggleGroup QualidadeAtendimento;
 
-    @FXML
-    private ToggleGroup NotaAtendimento;
+	    @FXML
+	    private RadioButton rbBomESimples;
 
-    @FXML
-    private ToggleGroup AparenciaExterna;
+	    @FXML
+	    private RadioButton rbOtimoEducado;
 
-    @FXML
-    private ToggleGroup NotaAspectos;
+	    @FXML
+	    private RadioButton rbRegular;
 
-    @FXML
-    private ToggleGroup OpcoesPagamento;
+	    @FXML
+	    private RadioButton rbComEsperaComReserva;
 
-    @FXML
-    void Voltar(ActionEvent event) {
+	    @FXML
+	    private ToggleGroup Espera;
 
-    }
+	    @FXML
+	    private RadioButton rbSemEsperaComReserva;
 
-    @FXML
-    void Salvar(ActionEvent event) {
+	    @FXML
+	    private RadioButton rbSemEsperaSemReserva;
 
-    }
-  
+	    @FXML
+	    private RadioButton rbLimpo;
+
+	    @FXML
+	    private RadioButton rbTematica;
+
+	    @FXML
+	    private ToggleGroup AparenciaInterna;
+
+	    @FXML
+	    private ToggleGroup NotaAspectos;
+
+	    @FXML
+	    private RadioButton rbPremium;
+
+	    @FXML
+	    private ToggleGroup AparenciaExterna;
+
+	    @FXML
+	    private RadioButton rbOrganizadoeRustico;
+
+	    @FXML
+	    private ToggleGroup Limpeza;
+
+	    @FXML
+	    private RadioButton rbLimpoBrilhando;
+
+	    @FXML
+	    private RadioButton rbBoa;
+
+	    @FXML
+	    private RadioButton rbDiscreta;
+
+	    @FXML
+	    private RadioButton rbRuim;
+
+	    @FXML
+	    private RadioButton rbInadequado;
+
+	    @FXML
+	    private RadioButton rbSucos;
+
+	    @FXML
+	    private ToggleGroup BebidasSemAlcool;
+
+	    @FXML
+	    private RadioButton rbPoucas;
+
+	    @FXML
+	    private ToggleGroup BebidasAlcoolicas;
+
+	    @FXML
+	    private ToggleGroup NotaCardapio;
+
+	    @FXML
+	    private RadioButton rbEspecifica;
+
+	    @FXML
+	    private ToggleGroup Variedade;
+
+	    @FXML
+	    private RadioButton rbNenhuma;
+
+	    @FXML
+	    private RadioButton rbSucosRefrigerantesETC;
+
+	    @FXML
+	    private RadioButton rbVariedadesDeBebidas;
+
+	    @FXML
+	    private RadioButton rbGrandeVariedade;
+
+	    @FXML
+	    private RadioButton rbAlgumasAprimoradas;
+
+	    @FXML
+	    private RadioButton rbRefrigerantes;
+
+	    @FXML
+	    private ToggleGroup Pagamento;
+
+	    @FXML
+	    private ToggleGroup QualidadeValorPago;
+
+	    @FXML
+	    private RadioButton rbCaro;
+
+	    @FXML
+	    private RadioButton rbCartaoDeDebito;
+
+	    @FXML
+	    private ToggleGroup OpcoesPagamento;
+
+	    @FXML
+	    private RadioButton rbDinheiro;
+
+	    @FXML
+	    private RadioButton rbCartaoDeCrediro;
+
+	    @FXML
+	    private RadioButton rbBom;
+
+	    @FXML
+	    private RadioButton rbBarato;
+
+	    @FXML
+	    private TextArea taDescricao;
+
+	    @FXML
+	    private Button btnVoltar;
+
+	    @FXML
+	    private Button btnSalvar;
+
+	    @FXML
+	    void Salvar(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void Voltar(ActionEvent event) {
+
+	    } 
     
     public AvaliacaoController() {
 		AvaliacaoDAO = DAOFactory.get().avaliacaoDAO();
