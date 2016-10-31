205ff6c7627d9c56a70bf022f236b5a1be1845b3
Lucas <Lucas@172.18.24.50>
2016-10-31 20:39:53 -0200

    Correção

--- restaurantesFamosos/src/main/java/controller/LoginController.java
+++ restaurantesFamosos/src/main/java/controller/LoginController.java
@@ -1,17 +1,23 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import conexao.Conexao;
+import conexao.ConexaoMysqlProducao;
+import dao.UsuarioDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
@@ -37,6 +43,15 @@ public class LoginController {
 
 	@FXML
 	private Button btnEntrar;
+	private Conexao log;
+
+	private UsuarioDAO usuarioDAO;
+
+	public LoginController() {
+		usuarioDAO = factory.DAOFactory.get().usuarioDAO();
+		log = new ConexaoMysqlProducao();
+		}
+
 
 	@FXML
 	void CriarConta(ActionEvent event) {
@@ -45,15 +60,36 @@ public class LoginController {
 
 	@FXML
 	void Entrar(ActionEvent event) throws SQLException {
-		if ("admin".equals(tfSenha.getText()) && "admin".equals(tfLogin.getText())) {
-			Alert alert = new Alert(AlertType.INFORMATION, "Login efetuado com sucesso!", ButtonType.CLOSE);
-			alert.show();
-			// abre a tela principal de seta o menu no topo
-			AbreTela("Principal.fxml");
-		} else {
-			Alert alert = new Alert(AlertType.ERROR, "Aten√ß√£o!! Os dados est√£o incorretos", ButtonType.CLOSE);
-			alert.show();
-		}
+		int compara = 1;
+		try {
+			Connection connection = log.get();
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("select Nome, senha from Usuario");
+			rs.first();
+			while (compara == 1) {
+				if (tfLogin.getText().equals("") || (tfSenha.getText().equals(""))) {
+					JOptionPane.showMessageDialog(null, "Campos n„o podem ser nulos.");
+					compara = 2;
+					break;
+				}
+				if (tfLogin.getText().equals(rs.getString("Nome"))
+						&& (tfSenha.getText().equals(rs.getString("Senha")))) {
+					JOptionPane.showMessageDialog(null, "Login correto! Aguarde o sistema abrir.");
+
+					AbreTela("Principal.fxml");
+					compara = 2;
+					break;
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Us·rio ou senha incorretos!");
+					compara = 2;
+					break;
+				}
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();}
+		
 	}
 
 	@FXML
