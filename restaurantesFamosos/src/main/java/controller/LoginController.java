d2875c7a13b03c7a32026705f207d1d166d36a9e
vagnertondello <vagnertondello@hotmail.com>
2016-10-31 19:57:37 -0200

    Commit css

--- restaurantesFamosos/src/main/java/controller/LoginController.java
+++ restaurantesFamosos/src/main/java/controller/LoginController.java
@@ -1,17 +1,23 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import conexao.Conexao;
+import conexao.ConexaoMysqlProducao;
+import dao.UsuarioDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
@@ -37,6 +43,15 @@ public class LoginController {
 
 	@FXML
 	private Button btnEntrar;
+	private Conexao log;
+
+	private UsuarioDAO usuarioDAO;
+
+	public LoginController() {
+		usuarioDAO = factory.DAOFactory.get().usuarioDAO();
+		log = new ConexaoMysqlProducao();
+		}
+
 
 	@FXML
 	void CriarConta(ActionEvent event) {
@@ -45,49 +60,6 @@ public class LoginController {
 
 	@FXML
 	void Entrar(ActionEvent event) throws SQLException {
-		if ("admin".equals(tfSenha.getText()) && "admin".equals(tfLogin.getText())) {
-			Alert alert = new Alert(AlertType.INFORMATION, "Login efetuado com sucesso!", ButtonType.CLOSE);
-			alert.show();
-			// abre a tela principal de seta o menu no topo
-			AbreTela("Principal.fxml");
-		} else {
-			Alert alert = new Alert(AlertType.ERROR, "Atenção!! Os dados estão incorretos", ButtonType.CLOSE);
-			alert.show();
-		}
-	}
-
-	@FXML
-	void LembrarSenha(ActionEvent event) {
-
-	}
-
-	@FXML
-	void EsqueciSenha(ActionEvent event) {
-		AbreTela("RecuperaSenha.fxml");
-	}
-
-	public void AbreTela(String tela) {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/telas/" + tela));
-		try {
-			AnchorPane telaView = (AnchorPane) loader.load();
-			panelPrincipal.setCenter(telaView);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	}
-
-}
-	private Conexao log;
-
-	private UsuarioDAO usuarioDAO;
-
-	public LoginController() {
-		usuarioDAO = factory.DAOFactory.get().usuarioDAO();
-		log = new ConexaoMysqlProducao();
-
-	}
-
 		int compara = 1;
 		try {
 			Connection connection = log.get();
@@ -116,5 +88,34 @@ public class LoginController {
 
 			}
 		} catch (SQLException e) {
-			e.printStackTrace();
+			e.printStackTrace();}
+		
+	}
+
+	@FXML
+	void LembrarSenha(ActionEvent event) {
+
+	}
+
+	@FXML
+	void EsqueciSenha(ActionEvent event) {
+		AbreTela("RecuperaSenha.fxml");
+	}
+
+	public void AbreTela(String tela) {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/telas/" + tela));
+		try {
+			AnchorPane telaView = (AnchorPane) loader.load();
+			panelPrincipal.setCenter(telaView);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
+	
+	
+
+		
 
